9cd44962801b9859a4742a547e311056e48fdf27
Nydus Born <90088743+NydusBorn@users.noreply.github.com>
2024-07-18 20:49:53 +0300

    clarfication: migration

--- asp-backend/asp-backend/Program.cs
+++ asp-backend/asp-backend/Program.cs
@@ -25,6 +25,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 var db = new UserContext();
+
+// Fine for in development, removes db to stay in compliance with current schema
+db.Database.EnsureDeleted();
 db.Database.EnsureCreated();
+
+// If modification of db is required in production, then use the Migrate method (you must consult ef core documentation on migrations,
+// and also, if you started with EnsureCreated, then you must roll your own migrations manually (first create a new db via migrations (that fits the schema), then use your code to migrate db to db))
+// db.Database.Migrate();
+
 
 app.Run();
