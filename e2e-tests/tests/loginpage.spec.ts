6ff510854cebc773cc28b3b395a18727922f56b7
Nydus Born <90088743+NydusBorn@users.noreply.github.com>
2024-08-24 18:18:32 +0300

    login page e2e tests complete

--- e2e-tests/tests/loginpage.spec.ts
+++ e2e-tests/tests/loginpage.spec.ts
@@ -11,10 +11,42 @@ test.describe("login page", () => {
     await page.goto("http://localhost:3000/login");
     await page.waitForLoadState("networkidle");
 
-    await page.fill("#username", "test");
+    await page.fill("#username", "guest");
 
     await page.click("#guest_button");
 
     await expect(page).toHaveURL("http://localhost:3000/lobby");
+    let lcp = await page.context().storageState();
+    let lca = lcp.origins[0].localStorage;
+    let lc = {};
+    lca.forEach((x) => {
+      lc[x.name] = x.value;
+    });
+    expect(lc).toHaveProperty("uid");
+
+    expect(lc).toHaveProperty("password");
+    expect(lc["password"].length).toBe(16);
+  });
+
+  test("user register", async ({ page }) => {
+    await page.goto("http://localhost:3000/login");
+    await page.waitForLoadState("networkidle");
+
+    await page.fill("#username", "user");
+    await page.locator(".p-password-input").pressSequentially("testpass");
+
+    await page.click("#user_button");
+
+    await expect(page).toHaveURL("http://localhost:3000/lobby");
+    let lcp = await page.context().storageState();
+    let lca = lcp.origins[0].localStorage;
+    let lc = {};
+    lca.forEach((x) => {
+      lc[x.name] = x.value;
+    });
+    expect(lc).toHaveProperty("uid");
+
+    expect(lc).toHaveProperty("password");
+    expect(lc["password"].length).toBe(8);
   });
 });
